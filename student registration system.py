c88d0a58fa285224ea41adf44944951b13bc3a96
Pablo <pabloquiroga0987@gmail.com>
2026-08-24 11:42:58 -0500

    Initialize student registration system

---
+++ student registration system.py
@@ -0,0 +1,7 @@
+estudiantes = []
+while True:
+  print("\n---SISTEMA DE ESTUDIANTES---")
+  print("1. REGISTRAR ESTUDIANTE") 
+  print("2. VER ESTUDIANTES")
+  print("3. BUSCAR ESTUDIANTES")
+  print("4. SALIR)
